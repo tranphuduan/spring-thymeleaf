34d16977c1ca97bbd99173d367a28d42b09056ab
DuanTP <tranphuduan@gmail.com>
2024-09-21 22:16:04 +0700

    log

--- src/main/java/com/example/WebController.java
+++ src/main/java/com/example/WebController.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -18,6 +21,18 @@ public class WebController {
         log.info("request = "+ new Date());
         // Tạo ra thông tin
         List<Info> profile = new ArrayList<>();
+        String pattern = "MM/dd/yyyy HH:mm:ss";
+
+// Create an instance of SimpleDateFormat used for formatting
+// the string representation of date according to the chosen pattern
+        DateFormat df = new SimpleDateFormat(pattern);
+
+// Get the today date using Calendar object.
+        Date today = Calendar.getInstance().getTime();
+// Using DateFormat format method we can create a string
+// representation of a date with the defined format.
+        String todayAsString = df.format(today);
+        profile.add(new Info("Date", todayAsString));
         profile.add(new Info("fullname", "Nguyễn Hoàng Nam"));
         profile.add(new Info("nickname", "lốddaf"));
         profile.add(new Info("gmail", "loda.namnh@gmail.com"));
