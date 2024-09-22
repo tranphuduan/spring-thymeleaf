4201558fa46d7979cf88dac9f9c46e810694c9b4
DuanTP <tranphuduan@gmail.com>
2024-09-22 18:10:16 +0700

    index.html

--- src/main/java/com/example/WebController.java
+++ src/main/java/com/example/WebController.java
@@ -16,29 +16,29 @@ import java.util.List;
 @Slf4j
 public class WebController {
 
-    @GetMapping("/profile")
-    public String profile(Model model){
-        log.info("request = "+ new Date());
-        // Tạo ra thông tin
-        List<Info> profile = new ArrayList<>();
-        String pattern = "MM/dd/yyyy HH:mm:ss";
-
-// Create an instance of SimpleDateFormat used for formatting
-// the string representation of date according to the chosen pattern
-        DateFormat df = new SimpleDateFormat(pattern);
-
-// Get the today date using Calendar object.
-        Date today = Calendar.getInstance().getTime();
-// Using DateFormat format method we can create a string
-// representation of a date with the defined format.
-        String todayAsString = df.format(today);
-        profile.add(new Info("Date", todayAsString));
-
-
-        // Đưa thông tin vào Model
-        model.addAttribute("lodaProfile", profile);
-
-        // TRả về template profile.html
-        return "profile";
-    }
+//    @GetMapping("/profile")
+//    public String profile(Model model){
+//        log.info("request = "+ new Date());
+//        // Tạo ra thông tin
+//        List<Info> profile = new ArrayList<>();
+//        String pattern = "MM/dd/yyyy HH:mm:ss";
+//
+//// Create an instance of SimpleDateFormat used for formatting
+//// the string representation of date according to the chosen pattern
+//        DateFormat df = new SimpleDateFormat(pattern);
+//
+//// Get the today date using Calendar object.
+//        Date today = Calendar.getInstance().getTime();
+//// Using DateFormat format method we can create a string
+//// representation of a date with the defined format.
+//        String todayAsString = df.format(today);
+//        profile.add(new Info("Date", todayAsString));
+//
+//
+//        // Đưa thông tin vào Model
+//        model.addAttribute("lodaProfile", profile);
+//
+//        // TRả về template profile.html
+//        return "profile";
+//    }
 }
