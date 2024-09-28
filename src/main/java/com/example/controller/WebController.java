eedb735dd688736cece2c25a9f13e83e1a9a60b4
DuanTP <tranphuduan@gmail.com>
2024-09-28 17:15:35 +0700

    chia package

--- src/main/java/com/example/controller/WebController.java
+++ src/main/java/com/example/controller/WebController.java
@@ -24,19 +24,19 @@ public class WebController {
         List<IndexInfo> indexInfoList = getListData();
         IndexInfo info = indexInfoList.stream().filter(x -> x.getId().equals(id)).findFirst().get();
         model.addAttribute("info", info);
-        return "item-detail"; // Return the name of the Thymeleaf template (index.html)
+        return "client/item-detail"; // Return the name of the Thymeleaf template (home.html)
     }
 
     @GetMapping("/error")
     public String itemDetail(Model model) {
-        return "error"; // Return the name of the Thymeleaf template (index.html)
+        return "error"; // Return the name of the Thymeleaf template (home.html)
     }
 
     @GetMapping("/auth/login")
     public String login(Model model) {
         model.addAttribute("page", "login");
 //        model.addAttribute("userInfo", userInfo);
-        return "login"; // Return the name of the Thymeleaf template (index.html)
+        return "login"; // Return the name of the Thymeleaf template (home.html)
     }
 
     int x=1;
@@ -107,45 +107,45 @@ public class WebController {
         model.addAttribute("search", search==null?"":search);
         model.addAttribute("page", "home");
 
-        return "index"; // Return the name of the Thymeleaf template (index.html)
+        return "client/home"; // Return the name of the Thymeleaf template (home.html)
     }
 
 
     public List<IndexInfo> getListData() {
         List<IndexInfo> indexInfoList = new ArrayList<>();
-        indexInfoList.add(new IndexInfo("1","/img/img-03.jpg", "Clocks", "18 Oct 2020", "9,906 views"));
-        indexInfoList.add(new IndexInfo("2","/img/img-04.jpg", "Plants", "14 Oct 2020", "16,100 views"));
-        indexInfoList.add(new IndexInfo("3","/img/img-05.jpg", "Morning", "12 Oct 2020", "12,460 views"));
-        indexInfoList.add(new IndexInfo("4","/img/img-06.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("5","/img/img-01.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("6","/img/img-02.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("7","/img/img-07.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("8","/img/img-08.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("9","/img/img-09.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("19","/img/img-10.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("11","/img/img-11.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("12","/img/img-12.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("13","/img/img-13.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("14","/img/img-14.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("15","/img/img-15.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("16","/img/img-16.jpg", "dâuntp", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("1","/client/img/img-03.jpg", "Clocks", "18 Oct 2020", "9,906 views"));
+        indexInfoList.add(new IndexInfo("2","/client/img/img-04.jpg", "Plants", "14 Oct 2020", "16,100 views"));
+        indexInfoList.add(new IndexInfo("3","/client/img/img-05.jpg", "Morning", "12 Oct 2020", "12,460 views"));
+        indexInfoList.add(new IndexInfo("4","/client/img/img-06.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("5","/client/img/img-01.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("6","/client/img/img-02.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("7","/client/img/img-07.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("8","/client/img/img-08.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("9","/client/img/img-09.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("19","/client/img/img-10.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("11","/client/img/img-11.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("12","/client/img/img-12.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("13","/client/img/img-13.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("14","/client/img/img-14.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("15","/client/img/img-15.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("16","/client/img/img-16.jpg", "dâuntp", "10 Oct 2020", "11,402 views"));
 
-        indexInfoList.add(new IndexInfo("17","/img/img-03.jpg", "Clocks-2", "18 Oct 2020", "9,906 views"));
-        indexInfoList.add(new IndexInfo("18","/img/img-04.jpg", "Plants-2", "14 Oct 2020", "16,100 views"));
-        indexInfoList.add(new IndexInfo("19","/img/img-05.jpg", "Morning", "12 Oct 2020", "12,460 views"));
-        indexInfoList.add(new IndexInfo("20","/img/img-06.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("21","/img/img-01.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("22","/img/img-02.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("23","/img/img-07.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("24","/img/img-08.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("25","/img/img-09.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("26","/img/img-10.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("27","/img/img-11.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("28","/img/img-12.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("29","/img/img-13.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("30","/img/img-14.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("31","/img/img-15.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
-        indexInfoList.add(new IndexInfo("32","/img/img-16.jpg", "dâuntp", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("17","/client/img/img-03.jpg", "Clocks-2", "18 Oct 2020", "9,906 views"));
+        indexInfoList.add(new IndexInfo("18","/client/img/img-04.jpg", "Plants-2", "14 Oct 2020", "16,100 views"));
+        indexInfoList.add(new IndexInfo("19","/client/img/img-05.jpg", "Morning", "12 Oct 2020", "12,460 views"));
+        indexInfoList.add(new IndexInfo("20","/client/img/img-06.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("21","/client/img/img-01.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("22","/client/img/img-02.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("23","/client/img/img-07.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("24","/client/img/img-08.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("25","/client/img/img-09.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("26","/client/img/img-10.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("27","/client/img/img-11.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("28","/client/img/img-12.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("29","/client/img/img-13.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("30","/client/img/img-14.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("31","/client/img/img-15.jpg", "Pinky", "10 Oct 2020", "11,402 views"));
+        indexInfoList.add(new IndexInfo("32","/client/img/img-16.jpg", "dâuntp", "10 Oct 2020", "11,402 views"));
 
         return indexInfoList;
     }
